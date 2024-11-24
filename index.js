43625b82c90e36c4ee329f8ea12c2f4ba276cf00
lucassabino1998 <lucas.pirata17@gmail.com>
2024-11-24 14:01:26 -0300

    primeiro commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('ola mundo')
+
+let Mensagem = ' atacou usando ';
+class Heroi{
+    constructor(nome, idade ,tipo){
+        this.nome=nome;
+        this.idade=idade;
+        this.tipo=tipo;
+    }
+
+}
+
+let ataque = new Heroi ('lucas', 26, 'ninja');
+
+if(ataque.tipo==='mago'){
+    console.log('o '+ataque.tipo+Mensagem+ 'magia');
+}else if(ataque.tipo==='guerreiro'){
+    console.log('o '+ataque.tipo+Mensagem+ 'espada');
+}else if(ataque.tipo===' monge'){
+    console.log('o '+ataque.tipo+Mensagem+ 'artes marciais');
+}else{
+    console.log('o '+ataque.tipo+Mensagem+ 'shuriken');
+}
